382f7a36470d39e471d09607a000994848a1edc9
shubhadip-koley <koleyshubhadip2003@gmail.com>
2025-06-15 01:42:38 +0530

    first commit

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Kamarpukur',
+    image: 'chatrooms/kamarpukur.jpg'
+  },
+  {
+    name: 'Durgapur',
+    image: 'chatrooms/durgapore.jpg'
+  },
+  {
+    name: 'Midnapore',
+    image: 'chatrooms/mednapore.jpg'
+  },
+  {
+    name: 'Budge Budge',
+    image: 'chatrooms/Budge.jpg'
+  }
+]
+
---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Shubhadip',
+    lastName: 'Koley',
+    statusText: 'Expert in python',
+    image: 'users/shubhadip.jpg'
+  },
+  {
+    name: 'Dipam',
+    lastName: 'Dey',
+    statusText: 'Expert in java',
+    image: 'users/dipam.jpg'
+  },
+  {
+    name: 'Sayantika',
+    lastName: 'Manna',
+    statusText: 'Expert in c++',
+    image: 'users/sayantika.jpg'
+  },
+  {
+    name: 'Tanujit',
+    lastName: 'Banerjee',
+    statusText: 'Expert in javascript',
+    image: 'users/tanujit.jpg'
+  }
+]
+
